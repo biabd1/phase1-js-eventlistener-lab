1e2b49a5bed5dd6b3df6bbb83aab402e56783fd1
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-23 10:11:42 +0300

    DONE

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    function addingEventListener() {
+        const input = document.getElementById('button');
+    
+         input.addEventListener('click', function(){
+            alert("binds an event listener in addingEventListener()")
+         });
+    }
+    
+    addingEventListener();
 }
